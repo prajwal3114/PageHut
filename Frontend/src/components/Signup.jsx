ee20e58df6014e9f8462d99fc75e4914ef35fcf7
PRAJAWAL G <prajwal.gaddle@gmail.com>
2025-09-01 13:17:19 +0530

    Added authentication to login and sign up page

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,60 +1,94 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Login from './Login'
+import { useForm } from "react-hook-form"
 
 function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
-     <>
+    <>
       <div className='flex h-screen items-center justify-center '>
         <div id="my_modal_3" className="border-[2px] shadow-md p-5 rounded-md">
           <div className="">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* Close button */}
               <Link to='/' className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
+          
+              <h3 className="font-bold text-lg text-left">Signup</h3>
+              <div className='text-left'>
+                {/* Name */}
+                <div className='mt-4 space-y-2'>
+                  <span>Name</span>
+                  <br />
+                  <input 
+                    type="text" 
+                    placeholder='Enter your fullname' 
+                    className='w-80 px-3 py-1 border rounded-md outline-none'
+                    {...register("name", { required: true })}
+                  />
+                  <br />
+                  {errors.name && (
+                    <span className="text-sm text-red-500">
+                      This field is required
+                    </span>
+                  )}
+                </div>
+
+                {/* Email */}
+                <div className='mt-4 space-y-2'>
+                  <span>Email</span>
+                  <br />
+                  <input 
+                    type="email" 
+                    placeholder='Enter your email' 
+                    className='w-80 px-3 py-1 border rounded-md outline-none'
+                    {...register("email", { required: true })}
+                  />
+                  <br />
+                  {errors.email && (
+                    <span className="text-sm text-red-500">
+                      This field is required
+                    </span>
+                  )}
+                </div>
+
+                {/* Password */}
+                <div className='mt-4 space-y-2'>
+                  <span>Password</span>
+                  <br />
+                  <input 
+                    type="password" 
+                    placeholder='Enter your password' 
+                    className='w-80 px-3 py-1 border rounded-md outline-none'
+                    {...register("password", { required: true })}
+                  />
+                  <br />
+                  {errors.password && (
+                    <span className="text-sm text-red-500">
+                      This field is required
+                    </span>
+                  )}
+                </div>
+
+                {/* Button */}
+                <div className='flex justify-around mt-4'>
+                  <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200'>
+                    Signup
+                  </button>
+                </div>
+              </div>
             </form>
-            <h3 className="font-bold text-lg text-left">Signup</h3>
-            <div className='text-left'>
-                {/* email */}
-              <div className='mt-4 space-y-2'>
-                <span>Name</span>
-                <br />
-                <input 
-                  type="text" 
-                  placeholder='Enter your fullname' 
-                  className='w-80 px-3 py-1 border rounded-md outline-none'
-                />
-              </div>
-              {/* email */}
-              <div className='mt-4 space-y-2'>
-                <span>Email</span>
-                <br />
-                <input 
-                  type="email" 
-                  placeholder='Enter your email' 
-                  className='w-80 px-3 py-1 border rounded-md outline-none'
-                />
-              </div>
-              {/* Password */}
-              <div className='mt-4 space-y-2'>
-                <span>Password</span>
-                <br />
-                <input 
-                  type="password" 
-                  placeholder='Enter your password' 
-                  className='w-80 px-3 py-1 border rounded-md outline-none'
-                />
-              </div>
-              {/* Button */}
-              <div className='flex justify-around mt-4'>
-                <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200'>
-                  Signup
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
-     </>
+    </>
   )
 }
 
