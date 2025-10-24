6ad79726138512d69289bd05edcadddd6a53b71d
PRAJAWAL G <prajwal.gaddle@gmail.com>
2025-10-24 18:26:50 +0530

    Worked on the functionalites of the backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
-import express from 'express'
-import dotenv from 'dotenv'
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bookRoute from "../backend/route/book.route.js"
+
 dotenv.config();
-const PORT=process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('saurb randi')
-})
+const app = express();
 
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MONGO_URI;
+
+app.use(express.json());
+
+(async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+  }
+})();
+app.use("/book",bookRoute);
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Server is listening on ${PORT}`);
+});
