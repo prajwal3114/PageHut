e4cf9375640f0cef7e059b864097e459c396b4e4
PRAJAWAL G <prajwal.gaddle@gmail.com>
2025-10-27 14:08:27 +0530

    Backen completed

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   try {
@@ -9,14 +10,43 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    const hashPassword = await bcryptjs.hash(password, 10);
     const createdUser = new User({
       fullname,
       email,
-      password,
+      password: hashPassword,
     });
 
     await createdUser.save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log("Error: " + error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Username || Password" });
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Username || Password" });
+    }
+
+    return res.status(200).json({
+      message: "Login Successful",
+      user: {
+        _id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+      },
+    });
   } catch (error) {
     console.log("Error: " + error.message);
     res.status(500).json({ message: "Internal server error" });
